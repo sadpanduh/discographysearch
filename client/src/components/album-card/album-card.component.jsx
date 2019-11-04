10c30542ef917cfdae0e72a57b859087b1b09c6f
Anthony <noneedforemail>
2019-11-04 11:18:30 -0800

    adjust collection and card formatting

--- client/src/components/album-card/album-card.component.jsx
+++ client/src/components/album-card/album-card.component.jsx
@@ -6,8 +6,8 @@ const AlbumCard = ({albumcover, albumtype, albumtitle, albumdate}) =>(
     <div className='albumcard'>
         <img className='albumcover' src={albumcover} />
         <div className='albuminfo'>
-            <div className='albumtype'>Type:{albumtype}</div>
             <div className='albumtitle'>{albumtitle}</div>
+            <div className='albumtype'>Type: {albumtype}</div>
             <div className='albumdate'>Released:{albumdate}</div>
         </div>
     </div>
